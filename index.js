1df516a4a964f7810576c1ffdef30b18c847bb30
RekkyRek <haslun16@edu.vilhelmina.se>
2018-05-25 22:12:52 +0200

    Started implementing store

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ bot.register({name: 'parse', description: 'A command to demonstrate argument par
 
     msg.channel.createMessage(str)
 })
+
+bot.register({name: 'ping', description: 'Test the current ping to Discord API', usage: []}, async (args, msg) => {
+	let time = Date.now()
+    let message = await msg.channel.createMessage('Pinging...')
+    time = Date.now() - time
+    message.edit(`**A2R Time:** ${time}ms`)
+})
+
+bot.alias('p', 'ping')
