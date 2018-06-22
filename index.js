748e7f25b2d5306101a279e9ce4d8249d243c307
RekkyRek <haslun16@edu.vilhelmina.se>
2018-06-22 23:46:57 +0200

    added path resolve to store

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const THOT = require('./lib')
 
 const bot = new THOT(process.argv[2])
 
+bot.store.set('some/path/here', 'test')
+
+console.log(bot.store.get('some/path/here'))
+
 bot.register({name: 'parse', description: 'A command to demonstrate argument parsing', usage: ['"arguments go"','here']}, (args, msg) => {
     let str = ''
     str += `parsed arguments: [\n`
