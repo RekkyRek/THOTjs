216841780841da45279eab3c2cbc2dd997680443
RekkyRek <haslun16@edu.vilhelmina.se>
2018-06-22 23:20:44 +0200

    Added number parsing

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const bot = new THOT(process.argv[2])
 
 bot.register({name: 'parse', description: 'A command to demonstrate argument parsing', usage: ['"arguments go"','here']}, (args, msg) => {
     let str = ''
-    str += `parsed arguments: ${JSON.stringify(args)}`
+    str += `parsed arguments: [\n`
+
+    args.forEach(arg => {
+        str += ` (${typeof(arg)}) ${JSON.stringify(arg)},\n`
+    });
+
+    str += ']'
 
     msg.channel.createMessage(str)
 })
